56c5f0e56f23752947af5001a21c26a001b878f1
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-15 19:54:00 +0700

    feat: setup apollo graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Routes from "routes";
+import { ApolloProvider } from "@apollo/client";
+import client from "graphql/client";
 
 function App() {
-  return <Routes />;
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ src/graphql/client.ts
@@ -0,0 +1,30 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// https://www.apollographql.com/docs/react/pagination/core-api/
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        pokemons: {
+          keyArgs: [],
+          merge(existing, incoming, { args: { offset = 0 } }: any) {
+            if (!existing) return incoming;
+            const merged = existing.results.slice(0);
+            for (let i = 0; i < incoming.results.length; ++i) {
+              merged[offset + i] = incoming.results[i];
+            }
+
+            return { ...existing, results: merged };
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokeapi.graphcdn.app",
+  cache,
+});
+
+export default client;
