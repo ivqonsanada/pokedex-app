1cfc39c97d1c5daf0b5cbf6807733c1401aae09c
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-15 16:01:03 +0700

    feat: update top bar to use correct cursor and hide heart icon

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -3,7 +3,7 @@
 import { css } from "@emotion/react";
 import { Icon } from "@iconify/react";
 import chevronLeft from "@iconify/icons-akar-icons/chevron-left";
-import heartFilled from "@iconify/icons-ant-design/heart-filled";
+// import heartFilled from "@iconify/icons-ant-design/heart-filled";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { useWindowScrollPositions } from "hooks/useWindowScrollPositions";
@@ -27,7 +27,7 @@ const TopBar = () => {
     alignItems: "center",
     backgroundColor: scrollY === 0 ? "rgb(15 23 42)" : "rgb(51 65 85)",
     opacity: 1,
-    padding: "16px 0",
+    padding: "8px 0",
     width: "100%",
     maxWidth: "414px",
     margin: 0,
@@ -35,19 +35,26 @@ const TopBar = () => {
 
   const textStyle = css({
     fontWeight: "bold",
+    fontSize: "1.5em",
   });
 
   const iconStyle = css({
     width: "1.5em",
     height: "1.5em",
+    padding: "0.75em",
     // position: "absolute",
     top: 0,
   });
 
-  const heartIconStyle = css({
-    color: "white",
-    opacity: 0.4,
+  const backIconStyle = css({
+    cursor: "pointer",
   });
+
+  //   const heartIconStyle = css({
+  //     color: "white",
+  //     opacity: 0.4,
+  //     cursor: "pointer",
+  //   });
 
   useEffect(() => {
     console.log(location);
@@ -63,12 +70,17 @@ const TopBar = () => {
     <div css={baseStyle}>
       <p css={containerStyle}>
         {isDeepPath ? (
-          <Icon icon={chevronLeft} css={iconStyle} onClick={handleBack} />
+          <Icon
+            icon={chevronLeft}
+            css={[iconStyle, backIconStyle]}
+            onClick={handleBack}
+          />
         ) : (
           <span css={iconStyle}></span>
         )}
         <span css={textStyle}>Pokédex</span>
-        <Icon icon={heartFilled} css={[iconStyle, heartIconStyle]} />
+        {/* <Icon icon={heartFilled} css={[iconStyle, heartIconStyle]} /> */}
+        <span css={iconStyle}></span>
       </p>
     </div>
   );
