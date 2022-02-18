a23ad5472e6840ee0c96fee4636a6ced7f412eda
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-18 23:27:19 +0700

    feat: add react hot toast for feeback on save pokemon and release pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Routes from "routes";
 import { ApolloProvider } from "@apollo/client";
 import client from "graphql/client";
 import { PokemonProvider } from "contexts/pokemon-context";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <PokemonProvider>
+        <Toaster />
         <Routes />
       </PokemonProvider>
     </ApolloProvider>
--- src/contexts/pokemon-context.tsx
+++ src/contexts/pokemon-context.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 interface PokemonContextInterface {
   myPokemons: [];
@@ -21,12 +22,44 @@ const PokemonProvider: React.FC = (props) => {
     const newMyPokemons = [...myPokemons, newPokemon];
     setMyPokemons(newMyPokemons);
     localStorage.setItem(storageKey, JSON.stringify(newMyPokemons));
+    toast(
+      (t) => (
+        <span>
+          Yay! <b>{nickname}</b> now become your partner!
+        </span>
+      ),
+      {
+        duration: 4000,
+        position: "top-center",
+        style: {
+          borderRadius: "10px",
+          background: "rgb(30 41 59)",
+          color: "rgb(203 213 225)",
+        },
+      }
+    );
   };
 
   const releasePokemon = (nickname: string) => {
     const filteredMyPokemons = myPokemons.filter((e: any) => e.nickname !== nickname);
     setMyPokemons(filteredMyPokemons);
     localStorage.setItem(storageKey, JSON.stringify(filteredMyPokemons));
+    toast(
+      (t) => (
+        <span>
+          Nooo! <b>{nickname}</b> sad because you release it to the wild once more. :(
+        </span>
+      ),
+      {
+        duration: 8000,
+        position: "top-center",
+        style: {
+          borderRadius: "10px",
+          background: "rgb(30 41 59)",
+          color: "rgb(203 213 225)",
+        },
+      }
+    );
   };
 
   useEffect(() => {
