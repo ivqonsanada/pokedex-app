3e5a25059d4b1afc55a74bf90c32e53f63def7e5
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-19 00:12:33 +0700

    feat: add no pokemon state on my pokemon list page

--- src/pages/my-pokemon-list.tsx
+++ src/pages/my-pokemon-list.tsx
@@ -1,12 +1,65 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
 import Container from "components/layout/container";
 import MyPokemonCardList from "components/my-pokemon-card-list";
 import { usePokemon } from "contexts/pokemon-context";
+import { Link } from "react-router-dom";
 
 const MyPokemonList = () => {
   const { myPokemons } = usePokemon();
 
+  const noPokemonStyle = css({
+    color: "rgb(100 116 139)",
+    height: "60vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    "> * + *": {
+      marginTop: "24px",
+    },
+    "> div": {
+      "> * + *": {
+        marginTop: "8px",
+      },
+    },
+    // padding: ""
+  });
+
+  const buttonStyle = css({
+    background: "rgb(51 65 85)",
+    border: "solid 2px transparent",
+    display: "flex",
+    padding: "12px 24px",
+    color: "rgb(226 232 240)",
+    fontWeight: "bold",
+    borderRadius: "999px",
+    cursor: "pointer",
+    ":hover": {
+      borderColor: "rgb(71 85 105)",
+    },
+  });
+
   return (
     <Container>
+      {myPokemons.length === 0 && (
+        <div css={noPokemonStyle}>
+          <img
+            src="https://cdn.statically.io/gh/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif"
+            alt="Charizard sprite"
+            width={160}
+            height={160}
+          />
+          <div>
+            <p>You don't have any pokemon yet.</p>
+            <p>Lets catch some!</p>
+          </div>
+          <Link to="/" css={buttonStyle}>
+            Show me the way!
+          </Link>
+        </div>
+      )}
       <MyPokemonCardList data={myPokemons} />
     </Container>
   );
