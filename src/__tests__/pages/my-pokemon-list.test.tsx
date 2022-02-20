7fc2d835bda2193793d20bdf77d3fa1bc8daeff7
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-20 14:26:29 +0700

    test: my pokemon list page

---
+++ src/__tests__/pages/my-pokemon-list.test.tsx
@@ -0,0 +1,38 @@
+import { screen, within } from "@testing-library/react";
+import { myPokemons } from "mocks/data/my-pokemons";
+import { renderWithRoute } from "mocks/renders";
+import MyPokemonListPage from "pages/my-pokemon-list";
+
+const view = renderWithRoute(<MyPokemonListPage />);
+
+test("render top bar", () => {
+  view();
+  expect(screen.getByText(/pokédex app/i)).toBeVisible();
+});
+
+test("render with no pokemon", () => {
+  view();
+  expect(screen.getByText(/you don't have any pokemon yet\./i)).toBeVisible();
+  expect(screen.getByRole("link", { name: /show me the way!/i })).toBeVisible();
+});
+
+test("render with pokemon", () => {
+  localStorage.setItem("my-pokemons", JSON.stringify(myPokemons));
+  view();
+
+  const link = screen.getByRole("link", {
+    name: /bulbasaur sprite #001 bulbasaur si ceria release/i,
+  });
+  within(link).getByRole("button", { name: /release/i });
+
+  expect(screen.getByText(/teik/i)).toBeVisible();
+  expect(screen.getByText(/miii/i)).toBeVisible();
+  expect(screen.getByText(/iiin/i)).toBeVisible();
+  expect(screen.getByText(/senpai/i)).toBeVisible();
+  expect(screen.getByText(/mewtwo/i)).toBeVisible();
+});
+
+test("render nav menu", () => {
+  view();
+  expect(screen.getByText(/my pokémon/i)).toBeVisible();
+});
