0428ca0aaf887251e8b43c2ba1b4ff19335ecc0b
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-20 16:02:14 +0700

    chore: add my pokemons mock data

---
+++ src/mocks/data/my-pokemons.ts
@@ -0,0 +1,47 @@
+export const myPokemons = [
+  {
+    id: "1",
+    name: "bulbasaur",
+    sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    spriteAnimated:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif",
+    nickname: "si ceria",
+  },
+  {
+    id: "4",
+    name: "charmander",
+    sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    spriteAnimated:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif",
+    nickname: "teik",
+  },
+  {
+    id: "5",
+    name: "charmeleon",
+    sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+    spriteAnimated:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif",
+    nickname: "miii",
+  },
+  {
+    id: "6",
+    name: "charizard",
+    sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+    spriteAnimated:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif",
+    nickname: "iiin",
+  },
+  {
+    id: "150",
+    name: "mewtwo",
+    sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
+    spriteAnimated:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/150.gif",
+    nickname: "senpai",
+  },
+];
