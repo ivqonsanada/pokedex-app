f4fad8cc1e4877c668d52a4419527d984e6731c4
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-20 13:34:02 +0700

    chore: cleanup useHover hooks

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -6,19 +6,17 @@ export const useHover = <T>(): [MutableRefObject<T>, boolean] => {
   const ref: any = useRef<T | null>(null);
   const handleMouseOver = (): void => setValue(true);
   const handleMouseOut = (): void => setValue(false);
-  useEffect(
-    () => {
-      const node: any = ref.current;
-      if (node) {
-        node.addEventListener("mouseover", handleMouseOver);
-        node.addEventListener("mouseout", handleMouseOut);
-        return () => {
-          node.removeEventListener("mouseover", handleMouseOver);
-          node.removeEventListener("mouseout", handleMouseOut);
-        };
-      }
-    },
-    [ref.current] // Recall only if ref changes
-  );
+  useEffect(() => {
+    const node: any = ref.current;
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+      return () => {
+        node.removeEventListener("mouseover", handleMouseOver);
+        node.removeEventListener("mouseout", handleMouseOut);
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref.current]);
   return [ref, value];
 };
