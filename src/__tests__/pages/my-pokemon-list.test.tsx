1939bcb63fa061c21ed5ad4a40f5bc3cc6b4724e
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-20 16:12:20 +0700

    test: release pokemon on my pokemon page

--- src/__tests__/pages/my-pokemon-list.test.tsx
+++ src/__tests__/pages/my-pokemon-list.test.tsx
@@ -1,4 +1,5 @@
 import { screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { myPokemons } from "mocks/data/my-pokemons";
 import { renderWithRoute } from "mocks/renders";
 import MyPokemonListPage from "pages/my-pokemon-list";
@@ -30,6 +31,17 @@ test("render with pokemon", () => {
   expect(screen.getByText(/iiin/i)).toBeVisible();
   expect(screen.getByText(/senpai/i)).toBeVisible();
   expect(screen.getByText(/mewtwo/i)).toBeVisible();
+});
+
+test("release pokemon", async () => {
+  localStorage.setItem("my-pokemons", JSON.stringify(myPokemons));
+  view();
+  const link = screen.getByRole("link", {
+    name: /bulbasaur sprite #001 bulbasaur si ceria release/i,
+  });
+  const releaseButton = within(link).getByRole("button", { name: /release/i });
+  userEvent.click(releaseButton);
+  expect(link).not.toBeVisible();
 });
 
 test("render nav menu", () => {
