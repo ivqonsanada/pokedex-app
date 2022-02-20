5ee37c7ace91bc4c53bf0e21585e6ec12d2580ce
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-20 22:02:10 +0700

    test: top bar component

---
+++ src/__tests__/components/top-bar.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import TopBar from "components/top-bar";
+import { BrowserRouter } from "react-router-dom";
+import Provider from "provider";
+import userEvent from "@testing-library/user-event";
+
+const view = (overrideProps = {}) =>
+  render(
+    <Provider>
+      <TopBar isDeepPath {...overrideProps} />
+    </Provider>,
+    {
+      wrapper: BrowserRouter,
+    }
+  );
+
+test("render with back button", () => {
+  view();
+  expect(screen.getByRole("heading", { name: /pokédex app/i })).toBeVisible();
+  const backButton = screen.getByRole("button", {
+    name: /back button/i,
+  });
+  expect(backButton).toBeVisible();
+  userEvent.click(backButton);
+});
+
+test("render without back button", () => {
+  view({ isDeepPath: false });
+  expect(screen.getByRole("heading", { name: /pokédex app/i })).toBeVisible();
+  expect(screen.queryByRole("button", { name: /back button/i })).not.toBeInTheDocument();
+});
+
+test("render after scroll", () => {
+  view({ isDeepPath: false });
+  window.scrollY = 1;
+  expect(screen.getByRole("heading", { name: /pokédex app/i })).toBeVisible();
+});
