a37e421fe5a30a0467ecc48cf6fd2b0c29514025
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2022-02-21 11:05:19 +0700

    feat: pokemon owned component on click show summary each pokemon owned

--- src/components/pokemon-owned/index.tsx
+++ src/components/pokemon-owned/index.tsx
@@ -1,32 +1,32 @@
 /** @jsxImportSource @emotion/react */
 
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { css } from "@emotion/react";
 import pokemonIcon from "@iconify/icons-simple-icons/pokemon";
 import { Icon } from "@iconify/react";
 
 import { usePokemon } from "contexts/pokemon-context";
 import { useWindowScrollPositions } from "hooks/useWindowScrollPositions";
+import OwnedModal from "./owned-modal";
 
 const PokemonOwned = () => {
   const { scrollY } = useWindowScrollPositions();
   const { myPokemons } = usePokemon();
+  const [showModal, setShowModal] = useState(false);
 
-  const ownedContainerStyle = css({
+  const containerStyle = css({
     position: "fixed",
     top: scrollY > 0 ? 0 : 60,
     width: "100%",
     maxWidth: "414px",
-    display: "inline-flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: scrollY === 0 ? "rgb(15 23 42)" : "rgba(71, 85, 105, 0.75)",
-    backdropFilter: "blur(10px)",
-    padding: "8px 16px",
-    borderRadius: "0.24em 0 0 0.24em",
-    margin: "8px 0 -16px",
     zIndex: 101,
-    marginTop: 0,
+    display: "flex",
+    flexDirection: "column",
+    margin: "0 0 -16px",
+    padding: "8px 16px",
+    backgroundColor:
+      scrollY === 0 && !showModal ? "rgb(15 23 42)" : "rgba(71, 85, 105, 0.75)",
+    backdropFilter: "blur(10px)",
     transition: "0.3s ease",
     overflow: "hidden",
     ":hover": {
@@ -34,12 +34,23 @@ const PokemonOwned = () => {
     },
   });
 
+  const ownedContainerStyle = css({
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    marginTop: 0,
+    border: "none",
+    color: "rgb(226 232 240)",
+    cursor: "pointer",
+  });
+
   const ownedStyle = css({
     display: "inline-flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
-    marginLeft: 96,
+    alignItems: showModal ? "flex-end" : "center",
+    marginLeft: 128,
     "span:last-of-type": {
       fontWeight: "bold",
       fontSize: "1.2em",
@@ -52,19 +63,24 @@ const PokemonOwned = () => {
 
   const pokemonIconStyle = css({
     position: "absolute",
-    transform: "translate(-60px, -4px)",
+    transform: "translate(-40px, -4px)",
   });
 
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    <Link to="/my-pokemon">
-      <p css={ownedContainerStyle}>
+    <div css={containerStyle}>
+      <button onClick={handleClick} css={ownedContainerStyle}>
         <Icon icon={pokemonIcon} width={96} height={96} css={pokemonIconStyle} />
         <span css={ownedStyle}>
           <span>Owned</span>
           <span>{myPokemons.length}</span>
         </span>
-      </p>
-    </Link>
+      </button>
+      {showModal && <OwnedModal />}
+    </div>
   );
 };
 
---
+++ src/components/pokemon-owned/owned-modal/index.tsx
@@ -0,0 +1,77 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+
+import { usePokemon } from "contexts/pokemon-context";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  sprite: string;
+  nickname: string;
+  handleRelease: (e: any, nickname: string) => void;
+}
+
+const OwnedModal = () => {
+  const { myPokemons } = usePokemon();
+  const asdf: Pokemon[] = myPokemons;
+
+  const groupBy = (array: Pokemon[], predicate: (v: Pokemon) => number) => {
+    const group = array.reduce((acc, value) => {
+      (acc[predicate(value)] ||= []).push(value);
+      return acc;
+    }, {} as { [key: string]: Pokemon[] });
+    const groupKeys = Object.keys(group);
+    return groupKeys.map((e) => group[e]);
+  };
+
+  let totalEachMyPokemons = groupBy(asdf, (e) => e.id);
+
+  const containerStyle = css({
+    margin: "20px 0 12px",
+    transition: "0.3s ease",
+  });
+
+  const totalEachContainerStyle = css({
+    display: "grid",
+    gridTemplateColumns: "1fr 2ch",
+    alignItems: "center",
+    textAlign: "right",
+    margin: "0 auto",
+    maxWidth: "19ch",
+    gap: 8,
+    div: {
+      display: "flex",
+      gridTemplateColumns: "1fr 1fr",
+      alignItems: "center",
+      "> * + *": {
+        marginLeft: 4,
+      },
+    },
+    p: {
+      textTransform: "capitalize",
+    },
+  });
+
+  return (
+    <>
+      <div css={containerStyle}>
+        <div>
+          {totalEachMyPokemons.map((e) => {
+            return (
+              <div css={totalEachContainerStyle}>
+                <div>
+                  <img src={e[0].sprite} alt={e[0].name + " sprite"} width={48} />
+                  <p>{e[0].name}</p>
+                </div>
+                <p>{e.length}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OwnedModal;
